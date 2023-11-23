df885b976d3395b2b0a4d4c59920e95b771b3228
Jia Xinglong <jiaxinglong1996@outlook.com>
2023-11-23 22:59:00 +0800

    跨合约获取利益相关者的信息

--- Web3_NST/contracts/Web3_NST.sol
+++ Web3_NST/contracts/Web3_NST.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.8;
+pragma solidity ^0.8.19;
 
 // 分账的目标地址：算法提出者，投资人、运营商或工程师，艺术家（NFT 资产持有者）
 
@@ -8,3 +8,27 @@ pragma solidity ^0.8.8;
 // 根据身份进行提款的功能
 
 // 需要用 Chainlink 来定期根据数据库提供份额信息
+
+// 定义一个与 Stakeholders 合约的 stakeholderInfo 相同的 struct
+struct stakeholderInfo {
+    string name;
+    address payable payee;
+    string asset;
+    bool approval;
+}
+
+interface IStakeholders {
+    function getStakeholderInfo() external view returns (stakeholderInfo memory);
+}
+
+contract Web3NST {
+    IStakeholders[] stakeholdersInterfaces;
+
+    function loadStakeholder(address _stakeholderAddress) public {
+        stakeholdersInterfaces.push(IStakeholders(_stakeholderAddress));
+    }
+
+    function retrieveStakeholderInfo(uint256 _index) public view returns (stakeholderInfo memory) {
+        return stakeholdersInterfaces[_index].getStakeholderInfo();
+    }
+}
